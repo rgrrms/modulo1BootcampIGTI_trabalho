3d515b75ba7a101eb0a296da58f7d2466214f526
Róger Ramos <roger.ramos@fundacred.org.br>
2020-07-18 19:27:58 -0300

    alterando a cor atravez dos ranges

---
+++ js/index.js
@@ -0,0 +1,51 @@
+window.addEventListener('load', start);
+
+var rangeRed = document.querySelector('#rangeRed');
+var rangeGreen = document.querySelector('#rangeGreen');
+var rangeBlue = document.querySelector('#rangeBlue');
+var inputRed = document.querySelector('#textRed');
+var inputGreen = document.querySelector('#textGreen');
+var inputBlue = document.querySelector('#textBlue');
+var redValue = 0;
+var greenValue = 0;
+var blueValue = 0;
+var gridColor = document.querySelector('#gridColor');
+
+function start() {
+  changeColor();
+
+  rangeRed.addEventListener('input', redRangeChangeValue);
+
+  rangeGreen.addEventListener('input', greenRangeChangeValue);
+
+  rangeBlue.addEventListener('input', blueRangeChangeValue);
+}
+
+function redRangeChangeValue(event) {
+  redValue = event.target.value;
+
+  inputRed.value = redValue;
+
+  changeColor();
+}
+
+function greenRangeChangeValue(event) {
+  greenValue = event.target.value;
+
+  inputGreen.value = greenValue;
+
+  changeColor();
+}
+
+function blueRangeChangeValue(event) {
+  blueValue = event.target.value;
+
+  inputBlue.value = blueValue;
+
+  changeColor();
+}
+
+function changeColor() {
+  gridColor.style.backgroundColor =
+    'rgb(' + redValue + ', ' + greenValue + ', ' + blueValue + ')';
+}
